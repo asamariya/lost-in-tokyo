f82f4b6bb5f3d08be0b392e8f8f8d2865371f555
Abhishek Samariya <asamariya@gmail.com>
2019-09-26 09:19:39 +1000

    changed path for logo image

--- js/app.js
+++ js/app.js
@@ -26,7 +26,7 @@ const Intro = () => (
 const NavItem = ({className, href, children, logo}) => (
   <li className={`mh2-ns f6 f4-l tc ${className}`}>
     <a className="white no-underline" href={href}>
-      {logo ? <img src="../images/logo.svg" className="db center logo" /> : children}
+      {logo ? <img src="images/logo.svg" className="db center logo" /> : children}
     </a>
   </li>
 );
